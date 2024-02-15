a1ad227d7c532ad3720e7fc02f10f5c460cff5f1
Mugisha8 <yvesmugisha8@gmail.com>
2024-02-15 18:02:29 +0200

    single blog page finilized

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const mobileMenu = document.querySelector(".mobileMenu");
-    const menuList = document.querySelector(".menu-list");
-  
-    mobileMenu.addEventListener("click", function () {
-      menuList.classList.toggle("active");
-    });
+  const mobileMenu = document.querySelector(".mobileMenu");
+  const menuList = document.querySelector(".menu-list");
+
+  mobileMenu.addEventListener("click", function () {
+    menuList.classList.toggle("active");
+    mobileMenu.classList.toggle("active");
   });
-  
+
+  document.querySelectorAll(".menu-list").forEach((n) =>
+    n.addEventListener("click", () => {
+      menuList.classList.remove("active");
+      mobileMenu.classList.remove("active");
+    })
+    
+  );
+});
